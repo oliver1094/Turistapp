db8466d2abfe95b2ada83106fd71e1d3715c6341
SkyN <SkyN@SkyN-PC>
2016-03-01 12:44:13 -0600

    Ver eventos principales (sin imagenes)

--- views/site/index.php
+++ views/site/index.php
@@ -1,52 +1,55 @@
 <?php
 
 /* @var $this yii\web\View */
-
+use app\models\CatEvent;
 $this->title = 'My Yii Application';
+
+//echo $arrayMainEvents[0]->i_Score;
+//echo $arrayMainEvents[1]->i_Score;
+//echo $arr//echo $arrayMainEvents[0]->i_Score;ayMainEvents[2]->i_Score;
+//echo $arrayMainEvents[3]->i_Score;
+//echo $arrayMainEvents[4]->i_Score;
+//echo $arrayMainEvents[5]->i_Score;
+//echo $arrayMainEvents[6]->i_Score;
+//echo $arrayMainEvents[7]->i_Score;
+//echo $arrayMainEvents[8]->i_Score;
+//echo $arrayMainEvents[9]->i_Score;
+
+
+//foreach ($arrayMainEvents as $value) {
+//    echo $value->i_Score;
+//}
+
 ?>
+
+
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <h1>Eventos principales</h1>
     </div>
 
     <div class="body-content">
 
         <div class="row">
+            
+            <?php 
+                $contador=1;
+                foreach ($arrayMainEvents as $value) { ?>
+                
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Evento <?php echo $contador ?></h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p><?php echo  $arrayMainEvents[$contador-1]->tx_DescriptionEvent ?></p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+            
+            <?php 
+                $contador++;
+                } ?>
+            
+            
         </div>
 
     </div>
